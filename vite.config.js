55f59b476329efbc4abea158ca8c1b124cd3eb83
powellnorma <101364699+powellnorma@users.noreply.github.com>
2023-05-11 21:26:15 +0200

    init

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+
+export default defineConfig({
+	plugins: [
+		svelte({
+			/* plugin options */
+			preprocess: [sveltePreprocess({})]
+		})
+	],
+
+	build: {
+		sourcemap: true,
+	},
+
+});
